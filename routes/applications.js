fcf80d3ee7d7aa82b661fe9964a94e5de9924e27
Tiran Wijerathne <tiranwijeratne@gmail.com>
2025-01-05 22:56:54 +0530

    application
    application connected to backend

--- routes/applications.js
+++ routes/applications.js
@@ -1,21 +1,69 @@
 const express = require('express');
 let application = require('../models/application_model');
 const router = express.Router();
+const app = express();
+app.use(express.json());
+router.use("/files", express.static("files"))
 
-//adding a application
-router.route("/Aadd").post((req, res) => {
-    const name = req.body.name;
+
+
+//multer
+const multer  = require('multer');
+const req = require('express/lib/request.js');
+const res = require('express/lib/response.js');
+// const Application = require('../models/application_model');
+const { default: mongoose } = require('mongoose');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./files")
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now()
+    cb(null, uniqueSuffix+file.originalname)
+  }
+})
+
+require('../models/application_model');
+const upload = multer({ storage: storage })
+
+
+router.post("/upload-files", upload.single("file"), async(req, res) => {
+  console.log(req.file);
+  const name = req.body.name;
     const email = req.body.email;
     const portfolio = req.body.portfolio;
     const phoneNo = req.body.hireType;
     const introduction= req.body.introduction;
+    const filename = req.file.filename;
+
+    try{
+        await Application.create({ name: name, email:email, portfolio: portfolio, phoneNo: phoneNo, introduction:introduction, filename: filename  });
+        res.send({status: "ok"});
+    }catch(error){
+        res.json({status:error})
+    }
+});
+
+
+
+
+//adding a application
+router.route("/Aadd").post(upload.single("file"), async(req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const portfolio = req.body.portfolio;
+    const phoneNo = req.body.phoneNo;
+    const introduction= req.body.introduction;
+    const filename = req.file.filename;
 
     const newApplication = new application({
         name,
         email,
         portfolio,
         phoneNo,
-        introduction
+        introduction,
+        filename
     });
 
     newApplication.save().then(() => {
