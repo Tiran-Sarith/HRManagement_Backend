c0d656e9eccce159a0f3f7b5ec75e878130cf813
Tiran-Sarith <tiranwijeratne@gmail.com>
2025-03-28 19:15:22 +0530

    Revert "Merge branch 'main' of https://github.com/Tiran-Sarith/HRManagement_Backend"
    This reverts commit 49de145848a638fe9a1d7e22bee94f1c8c1269d9, reversing
    changes made to 45dcf47ba0c77f7dbb892df9f93581863b04767c.

---
+++ routes/Users.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const User = require('../models/Users_model');
+const router = express.Router();
+
+// Add a user
+router.post('/add', async (req, res) => {
+    const { name, employeeId, role, email } = req.body;
+
+    const newUser = new User({
+        name,
+        employeeId,
+        role,
+        email
+    });
+
+    try {
+        await newUser.save();
+        res.status(201).send("User added successfully");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error adding user");
+    }
+});
+
+// Get all users
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        console.log("user",users)
+        res.status(200).json(users);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error fetching users");
+    }
+});
+
+// Get a specific user
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const user = await User.findById(id);
+        if (!user) return res.status(404).send("User not found");
+        res.status(200).json(user);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error fetching user");
+    }
+});
+
+/// Update a user
+router.put('/update/:id', async (req, res) => {
+    const { id } = req.params;
+    const { name, employeeId, role, email } = req.body;
+
+    // Validate input
+    if (!name || !employeeId || !role || !email) {
+        return res.status(400).send("All fields are required");
+    }
+
+    try {
+        await User.findByIdAndUpdate(id, {
+            name,
+            employeeId,
+            role,
+            email
+        });
+        res.status(200).send("User updated successfully");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error updating user");
+    }
+});
+
+// Delete a user
+router.delete('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        await User.findByIdAndDelete(id);
+        res.status(200).send("User deleted successfully");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error deleting user");
+    }  
+}
+);
+
+module.exports = router;
--- routes/signup.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const signupController = require('../Controllers/Signup');
-const router = express.Router();
-
-router.post("/register", signupController.createUser);
-
-module.exports = router;
